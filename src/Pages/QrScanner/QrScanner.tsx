11858c34f512949c321d09142b362b7b6948c11c
денис турков <turkov_d_v@mail.ru>
2022-12-05 21:35:25 +0300

    change scanner

--- src/Pages/QrScanner/QrScanner.tsx
+++ src/Pages/QrScanner/QrScanner.tsx
@@ -1,17 +1,28 @@
+import axios from "axios";
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
+import "./QrScanner.css";
 
+type Equipment = {
+  equipmentTableId: bigint;
+  title: string;
+  responsibleName: string;
+  avatar: string;
+};
 
 const QrScanner = () => {
-  const [data, setData] = useState("No result");
+  const [data, setData] = useState<Equipment>();
 
   return (
-    <div>
+    <div className="result">
       <QrReader
-        containerStyle={{width:'20rem'}}
+        containerStyle={{ width: "20rem" }}
         onResult={(result, error) => {
           if (!!result) {
-            setData(result?.getText());
+            if (!result.toString().includes("89.110.53.87")) setData(data);
+            else {
+              axios.get(result.toString()).then((resp) => setData(resp.data));
+            }
           }
           if (!!error) {
             console.log(error.message);
@@ -19,7 +30,15 @@ const QrScanner = () => {
         }}
         constraints={{ facingMode: "environment" }}
       />
-      <p>{data}</p>
+      {data == null ? (
+        <p>No result</p>
+      ) : (
+        <div className="result">
+          <p>{data.title}</p>
+          <p>{data.responsibleName}</p>
+          <img src={data.avatar} />
+        </div>
+      )}
     </div>
   );
 };
