c98e43677c4ce95d876212571e66bcee36848705
денис турков <turkov_d_v@mail.ru>
2022-12-05 21:39:53 +0300

    change scanner

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 var app = express();
 
 app.use(express.static(__dirname))
-app.user(express.static(path.resolve(__dirname,'build')))
+app.use(express.static(path.resolve(__dirname,'build')))
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'build','index.html'))
